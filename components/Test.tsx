bc68a17d2b9bc7463df2778b39e53e50a1db9987
Fabien Motte <fabien.motte@algolia.com>
2021-07-29 15:16:53 +0200

    chore: initial commit

---
+++ components/Test.tsx
@@ -0,0 +1,5 @@
+import styles from "./Test.module.css";
+
+export default function Test() {
+  return <div className={styles.test}>Test</div>;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import Test from "../components/Test.tsx";
+
+export default function IndexPage() {
+  return <Test />;
+}
